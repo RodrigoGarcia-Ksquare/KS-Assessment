5083803fe64d6f52b23bf9728c34bc9348547aef
RodrigoGarcia-Ksquare <al1609049@upy.edu.mx>
2023-02-01 10:28:10 -0600

    Contact picture logic

--- force-app/main/default/lwc/fetchContactsDetail/fetchContactsDetail.js
+++ force-app/main/default/lwc/fetchContactsDetail/fetchContactsDetail.js
@@ -5,11 +5,15 @@ const FIELDS = [
     'Contact.Name',
     'Contact.Phone',
     'Contact.Email',
+    'Contact.Picture__c'
 ];
 
 export default class FetchContactsDetail extends LightningElement {
 
     @api contactid;
+
+    @track URLpic;
+    @track URLcon;
 
     @wire(getRecord, { recordId: '$contactid', fields: FIELDS }) contact;
 
@@ -23,6 +27,18 @@ export default class FetchContactsDetail extends LightningElement {
 
     get email() {
         return this.contact.data.fields.Email.value;
+    }
+
+    get conurl(){
+        this.URLcon = 'https://theksquaregroup100-dev-ed.develop.lightning.force.com/lightning/r/Contact/'+this.contactid+'/view'
+        return this.URLcon;
+    }
+
+    get picture() {
+        this.URLpic = this.contact.data.fields.Picture__c.value;
+        this.URLpic = this.URLpic.split('"');
+        this.URLpic = 'https://theksquaregroup100-dev-ed.develop.lightning.force.com' + this.URLpic[1];
+        return this.URLpic;
     }
 
 }
